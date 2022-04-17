a9a11b1566de56d6289599dcaac327e4e1c2ac3b
luka.dimjasevic <luka.dimjasevic@gmail.com>
2022-04-17 22:54:47 +0200

    Dodajem mogućnost slanja svih izmjena samo onim korisnicima koji to žele

--- emailBot/rasporedMessage.js
+++ emailBot/rasporedMessage.js
@@ -8,69 +8,80 @@ query(mailUsers, function(error, result) {
     if (error) throw error;
 
     for (i in result) {
-        let j;
+        let j, countEmptyChanges = 0;
         let tableData = {};
         let classSchedule = {};
         
-        if (result[i].unsubscribed == 0) {  // provjera da li korisnik prima izmjene
+        if (result[i].unsubscribed == 0) {
             // korisnik prima izmjene
-            if (result[i].salji_sve == 0) {
-                // korisnik ne zeli da mu dolaze sve izmjene
-                tableData.receiverEmail = result[i].adresa;
-                tableData.classID = result[i].razred_id;
+            
+            tableData.receiverEmail = result[i].adresa;
+            tableData.classID = result[i].razred_id;
+            tableData.sendAll = result[i].salji_sve;
 
-                // query za general_razred 
-                generalClass = `SELECT * FROM general_razred WHERE id = '${tableData.classID}'`;
-                query(generalClass, function(error, result){
-                    if (error) throw error;
-                    
-                    tableData.className = result[0].ime;
-                    if (result[0].aktivan == 1) {
-                        console.log(`Razred ${result[0].ime} je aktivan.`);
+            // query za general_razred 
+            generalClass = `SELECT * FROM general_razred WHERE id = '${tableData.classID}'`;
+            query(generalClass, function(error, result){
+                if (error) throw error;
 
-                        // query za izmjene_razred
-                        classChanges = `SELECT id,razred_id,tablica_id,sat1,sat2,sat3,sat4,sat5,sat6,sat7,sat8,sat9 
-                                        FROM izmjene_razred WHERE razred_id = '${tableData.classID}'`;
-                        query(classChanges, function(error, result) {
+                tableData.className = result[0].ime;
+                if (result[0].aktivan == 1) {
+                    console.log(`Razred ${result[0].ime} je aktivan.`);
+
+                    // query za izmjene_razred
+                    classChanges = `SELECT id,razred_id,tablica_id,sat1,sat2,sat3,sat4,sat5,sat6,sat7,sat8,sat9 
+                                    FROM izmjene_razred WHERE razred_id = '${tableData.classID}'`;
+                    query(classChanges, function(error, result) {
+                        if (error) throw error;
+                        tableData.tableID = result[0].tablica_id;
+                        
+                        for (let j = 1; j < 10; j++)
+                            classSchedule[`sat${j}`] = result[0][`sat${j}`];
+
+                        tableData.scheduleChanges = classSchedule;
+
+                        // query za izmjene_tablica
+                        tableChanges = `SELECT * FROM izmjene_tablica WHERE id = '${tableData.tableID}'`;
+                        query(tableChanges, function(error, result) {
                             if (error) throw error;
-                            tableData.tableID = result[0].tablica_id;
                             
-                            for (let j = 1; j < 10; j++)
-                                classSchedule[`sat${j}`] = result[0][`sat${j}`];
+                            tableData.tableHeading = result[0].naslov;
+                            tableData.morning = result[0].prijepodne;
 
-                            tableData.scheduleChanges = classSchedule;
-                             
-                            // query za izmjene_tablica
-                            tableChanges = `SELECT * FROM izmjene_tablica WHERE id = '${tableData.tableID}'`;
-                            query(tableChanges, function(error, result) {
-                                if (error) throw error;
-                                
-                                tableData.tableHeading = result[0].naslov;
-                                tableData.morning = result[0].prijepodne;
+                            if (tableData.morning == 0) {
+                                j = -1;
+                                tableData.shiftHeading = "POSLIJEPODNE";
+                            } else {
+                                j = 1;
+                                tableData.shiftHeading = "PRIJEPODNE";
+                            }
 
-                                if (tableData.morning == 0) {
-                                    j = -1;
-                                    tableData.shiftHeading = "POSLIJEPODNE";
-                                } else {
-                                    j = 1;
-                                    tableData.shiftHeading = "PRIJEPODNE";
+                            for (change in tableData.scheduleChanges) {
+                                if (tableData.scheduleChanges[change] == null) {
+                                    countEmptyChanges++;
+                                } 
+                            }
+
+                            console.log(tableData);
+                            
+                            if (countEmptyChanges == 9) {
+                                if (tableData.sendAll == 1) {
+                                    // korisnik zeli sve izmjene
+                                    rasporedEmail.send_email(tableData, j);
                                 }
-                                
-                                console.log(tableData);
+                            } else {
+                                // korisnik ne zeli sve izmjene
                                 rasporedEmail.send_email(tableData, j);
-                            });    
-                        });
-                    } else {
-                        console.log(`Razred ${result[0].ime} nije aktivan.`);
-                    }
-                }); 
-            } else {
-                // korisnik zeli da mu dolaze sve izmjene
-
-            }
+                            }
+                        });   
+                    });
+                } else {
+                    console.log(`Razred ${result[0].ime} nije aktivan.`);
+                }
+            });
         } else { 
             // korisnik ne prima izmjene
-
+            console.log(`Korisnik ${result[0].adresa} ne prima izmjene.`)
         }
     }
 });
