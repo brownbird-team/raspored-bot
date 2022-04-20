13c2336546a84333125db4bd5f24b7a1c2067954
luka.dimjasevic <luka.dimjasevic@gmail.com>
2022-04-20 20:04:15 +0200

    Vraćam na prijašnju verziju

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const baza = require('./databaseConnect.js');
 
 async function retardFunction () {
-    let datum = "";
-    const d = new Date()
-    datum += d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    console.log(datum);
+    result = await baza.promiseQuery("SELECT * FROM general_razred");
+    console.log(result);
 }
 
 retardFunction();
