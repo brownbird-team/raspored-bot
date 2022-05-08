635a74bf1d35e2a57bb80d4a21b27fad73c76674
rdobovic <roko.dobovicnik@gmail.com>
2022-05-08 16:39:03 +0200

    Dodajem naredbu Edit
    Naredba ispisuje link za web sučelje (koje trenutno ne postoji)

---
+++ discordBot/commands/edit.js
@@ -0,0 +1,25 @@
+const { errorEmbed, normalEmbed } = require("../helperFunctionsDisc.js");
+
+module.exports = {
+    name: "edit",
+    admin: false,
+    dmOnly: false,
+
+    async execute(message) {
+        let embed = await normalEmbed(
+            'Link za Web sučelje Raspored Bota',
+            'Pomoću Web sučelja lakše i brže konfigurirajte bota za svoj server'
+        );
+        embed.setURL('https://brownbird.eu');
+
+        embed.addFields({
+            name: "Napomena !",
+            value: "Web sučelje je eksperimentalna funkcija koja još nije dostupna u ovoj verziji bota"
+        });
+        
+        await message.reply( {
+            embeds: [embed],
+            allowedMentions: { repliedUser: false }
+        });
+    }
+}
