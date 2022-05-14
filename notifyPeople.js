2271413c79016fc4558c7ad49ed629a1bb120f54
rdobovic <roko.dobovicnik@gmail.com>
2022-05-14 19:43:46 +0200

    Dodajem ime funkciji za obavjesti

--- notifyPeople.js
+++ notifyPeople.js
@@ -1,7 +1,7 @@
 discordNotify = require('./discordBot/checkForChanges.js').check;
 
 // Funkcija koju webScraper poziva kada primjeti da je došlo do izmjena
-module.exports = async () => {
+exports.uzbuna = async () => {
     // Pozovi funkciju za obavještavanje ljudi na discordu
     await discordNotify();
 }
