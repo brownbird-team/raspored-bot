1226f15f4ce69cfc3373dfd2cf54197d2cb2792f
luka.dimjasevic <luka.dimjasevic@gmail.com>
2022-05-14 20:26:50 +0200

    Dodajem funkciju koja brise tokene svakih 12h, dodajem novi router

--- emailBot/app.js
+++ emailBot/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 const handlebars = require('express-handlebars');
+const database = require('./rasporedEmailFunkcije'  );
 
 let path = require('path');
 
@@ -14,10 +15,36 @@ app.use(express.urlencoded( {extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({limit: '1mb'}));
 
+const step = 43200000;
+const checkTokenState = () => {
+    setTimeout(async () => {
+        let tokenList1 = await database.getTokens('mail_korisnici');
+        let tokenList2 = await database.getTokens('mail_privremeni_korisnici');
+        for (token in tokenList1) {
+            try {
+                await database.checkToken(tokenList1[token], 'mail_korisnici');
+            } catch {
+                continue;
+            }
+        }
+        for (token in tokenList2) {
+            try {
+                await database.checkToken(tokenList2[token], 'mail_privremeni_korisnici');
+                await database.deleteTempData(tokenList2[token]);                
+            } catch {
+                continue;
+            }
+        }
+        checkTokenState();
+    }, step);
+}
+checkTokenState();
+
 /*=============== API ROUTES ===============*/
 
 app.use('/home', require('./routes/home'));
 app.use('/subscribe', require('./routes/subscribe'));
+app.use('/settings', require('./routes/settings'));
 app.use('/unsubscribe', require('./routes/unsubscribe'));
 app.use('/*', require('./routes/page404'));
 
