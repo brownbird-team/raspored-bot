015fb7f3cb8b95d0404c7ae1debf082103228454
Borneto <bornaflinta2005@gmail.com>
2022-06-05 14:06:08 +0200

    Notify people

--- notifyPeople.js
+++ notifyPeople.js
@@ -3,5 +3,6 @@ discordNotify = require('./discordBot/checkForChanges.js').check;
 // Funkcija koju webScraper poziva kada primjeti da je došlo do izmjena
 exports.uzbuna = async () => {
     // Pozovi funkciju za obavještavanje ljudi na discordu
+    //console.log("Uzbuna -------------------------------------- Pozar")
     await discordNotify();
 }
