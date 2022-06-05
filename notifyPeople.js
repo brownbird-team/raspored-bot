cf2400e771e55309d071db232c5a46df285bb0cc
rdobovic <roko.dobovicnik@gmail.com>
2022-06-05 18:24:28 +0200

    Dajem klijenta funkciji (zaboravih prije)

--- notifyPeople.js
+++ notifyPeople.js
@@ -1,10 +1,11 @@
 discordNotify = require('./discordBot/checkForChanges.js');
+discordClient = require('./discordBot/main.js');
 emailNotify = require('./emailBot/rasporedMain.js');
 
 // Funkcija koju webScraper poziva kada primjeti da je došlo do izmjena
 exports.uzbuna = async () => {
     // Pozovi funkciju za obavještavanje ljudi na discordu
     //console.log("Uzbuna -------------------------------------- Pozar")
-    await discordNotify.check();
+    await discordNotify.check(discordClient.client);
     await emailNotify.main();
 }
