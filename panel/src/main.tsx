d481f5fd0d17a78a74b18c0917dd785b63762bed
Luka <luka.dimjasevic@gmail.com>
2023-03-28 21:38:17 +0200

    Dodajem početne fileove za panel

---
+++ panel/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
