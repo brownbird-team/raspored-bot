35a45b3914f8e03978e80f5240b75d9d9dfb3f04
lukadimjasevic <luka.dimjasevic@gmail.com>
2023-03-31 11:10:45 +0200

    Dodajem konfiguraciju za store

--- panel/src/main.tsx
+++ panel/src/main.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import "./assets/index.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
