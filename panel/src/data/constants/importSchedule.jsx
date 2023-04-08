fb4ccca059f96c0e6e31487f01c895a01414844d
lukadimjasevic <luka.dimjasevic@gmail.com>
2023-04-08 20:58:06 +0200

    Dodajem error-handling za komponentu ImportSchedule

---
+++ panel/src/data/constants/importSchedule.jsx
@@ -0,0 +1,9 @@
+// prettier-ignore
+const messages =
+{
+	NO_FILE_UPLOADED:	<>Nije odabrana niti jedna datoteka</>,
+	FILE_UPLOADED: 		<>Datoteka je uspješno prenešena</>,
+	NO_FILE_XML:		<>Datoteka nije <b className="highlight">.xml</b></>
+}
+
+export default messages;
--- panel/src/pages/ScheduleImport/ScheduleImport.jsx
+++ panel/src/pages/ScheduleImport/ScheduleImport.jsx
@@ -6,16 +6,23 @@ import ComponentBody from "../../components/ComponentBody";
 import ComponentFooter from "../../components/ComponentFooter";
 import PrimaryButton from "../../components/PrimaryButton";
 import SecondaryButton from "../../components/SecondaryButton";
+import Alert from "../../components/Alert";
+import alertMessages from "../../data/constants/importSchedule";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 // Definiraj Page Items za Header
 const pageItems = ["Uvezi raspored sati"];
 
 const ScheduleImport = () => {
+    // Definira state za otvaranja / zatvaranje
     const [openSteps, setOpenSteps] = useState(false);
 
+    // Definira state za unos rasporeda
     const [file, setFile] = useState(null);
     const fileInputRef = useRef(null);
+
+    // Definira state za alert poruku
+    const [alert, setAlert] = useState(null);
 
     const handleOnChange = () => {
         setFile(fileInputRef.current.files[0]);
@@ -25,13 +32,26 @@ const ScheduleImport = () => {
         fileInputRef.current.click();
     };
 
+    // Handler koji se pokreće kada korisnik pokuša uvesti raspored
     const handleSubmitClick = () => {
-        console.log(file);
+        // Dohvaća ekstenziju datoteke
+        const fileExtension = file.name.split(".").pop();
+
+        // Provjerava je li datoteka xml
+        if (fileExtension !== "xml") return setAlert({ type: "danger", message: alertMessages.NO_FILE_XML });
+        if (file) return setAlert({ type: "success", message: alertMessages.FILE_UPLOADED });
+        else return setAlert({ type: "danger", message: alertMessages.NO_FILE_UPLOADED });
     };
 
     return (
         <MainLayout pageItems={pageItems}>
             <div className="import-main">
+                {alert ? (
+                    <Alert type={alert.type} onClose={() => setAlert(null)}>
+                        {alert.message}
+                    </Alert>
+                ) : null}
+
                 <div className="import-steps" style={{ height: openSteps ? "15rem" : "3.5rem" }}>
                     <div className="import-steps-header icon" onClick={() => setOpenSteps(!openSteps)}>
                         <span>Koraci</span>
@@ -76,14 +96,18 @@ const ScheduleImport = () => {
                         />
                         {file ? (
                             <table className="file-table">
-                                <tr>
-                                    <th>Naziv datoteke</th>
-                                    <th>Veličina</th>
-                                </tr>
-                                <tr>
-                                    <td>{file.name}</td>
-                                    <td>{file.size / 1000} KB</td>
-                                </tr>
+                                <thead>
+                                    <tr>
+                                        <th>Naziv datoteke</th>
+                                        <th>Veličina</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{file.name}</td>
+                                        <td>{file.size / 1000} KB</td>
+                                    </tr>
+                                </tbody>
                             </table>
                         ) : (
                             <div className="import-file-info no-upload">
