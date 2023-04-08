c8f2cc428ab74f001c9efe29ba6a23d4e1b66ac4
lukadimjasevic <luka.dimjasevic@gmail.com>
2023-04-08 19:11:17 +0200

    Popravak u stilovima...

--- panel/src/pages/ScheduleImport/ScheduleImport.jsx
+++ panel/src/pages/ScheduleImport/ScheduleImport.jsx
@@ -1,11 +1,19 @@
 import React, { useRef, useState } from "react";
 import "./ScheduleImport.css";
 import MainLayout from "../../layouts/MainLayout";
+import ComponentHeader from "../../components/ComponentHeader";
+import ComponentBody from "../../components/ComponentBody";
+import ComponentFooter from "../../components/ComponentFooter";
+import PrimaryButton from "../../components/PrimaryButton";
+import SecondaryButton from "../../components/SecondaryButton";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 // Definiraj Page Items za Header
 const pageItems = ["Uvezi raspored sati"];
 
 const ScheduleImport = () => {
+    const [openSteps, setOpenSteps] = useState(false);
+
     const [file, setFile] = useState(null);
     const fileInputRef = useRef(null);
 
@@ -24,54 +32,72 @@ const ScheduleImport = () => {
     return (
         <MainLayout pageItems={pageItems}>
             <div className="import-main">
-                <div className="import-steps">
-                    <h5>Koraci:</h5>
-                    <ol>
-                        <li className="badge text-bg-secondary">
-                            1. Kliknite na <b>Odaberi</b> kako biste odabrali datoteku
-                        </li>
-                        <li className="badge text-bg-warning">
-                            <b>2. Datoteka mora biti isključivo .xml</b>
-                        </li>
-                        <li className="badge text-bg-secondary">
-                            3. Nakon što se .xml datoteka učita, kliknite na <b>Uvezi</b>
-                        </li>
-                    </ol>
-                </div>
-                <div className="import-body">
-                    <input
-                        type="file"
-                        id="schedule-import"
-                        accept="text/xml"
-                        ref={fileInputRef}
-                        onChange={handleOnChange}
-                        required
-                    />
-                    <div className="import-choose-file">
-                        <button type="button" className="btn btn-primary" onClick={handleButtonClick}>
-                            Odaberi
-                        </button>
-                        {file ? (
-                            <div className="import-file-info">
-                                <div>
-                                    <span>Filename:</span>
-                                    <span>{file.name}</span>
-                                </div>
-                                <div>
-                                    <span>Size:</span>
-                                    <span>{file.size / 1000} KB</span>
-                                </div>
+                <div className="import-steps" style={{ height: openSteps ? "15rem" : "3.5rem" }}>
+                    <div className="import-steps-header icon" onClick={() => setOpenSteps(!openSteps)}>
+                        <span>Koraci</span>
+                        <MdOutlineKeyboardArrowDown size={30} className={`icon ${openSteps ? "up" : "down"}`} />
+                    </div>
+                    {openSteps ? (
+                        <div className="import-steps-container">
+                            <div>
+                                <span>
+                                    1. Kliknite na <b className="highlight">Odaberi raspored</b> kako biste odabrali
+                                    datoteku
+                                </span>
                             </div>
+                            <div>
+                                <span>
+                                    2. <b className="highlight">Datoteka mora biti isključivo .xml</b>
+                                </span>
+                            </div>
+                            <div>
+                                <span>
+                                    3. Nakon što se .xml datoteka učita, kliknite na{" "}
+                                    <b className="highlight">Uvezi raspored</b>
+                                </span>
+                            </div>
+                        </div>
+                    ) : null}
+                </div>
+
+                <div className="import-body">
+                    <ComponentHeader>
+                        <span>Odabir rasporeda</span>
+                    </ComponentHeader>
+
+                    <ComponentBody>
+                        <input
+                            type="file"
+                            id="schedule-import"
+                            accept="text/xml"
+                            ref={fileInputRef}
+                            onChange={handleOnChange}
+                            required
+                        />
+                        {file ? (
+                            <table className="file-table">
+                                <tr>
+                                    <th>Naziv datoteke</th>
+                                    <th>Veličina</th>
+                                </tr>
+                                <tr>
+                                    <td>{file.name}</td>
+                                    <td>{file.size / 1000} KB</td>
+                                </tr>
+                            </table>
                         ) : (
                             <div className="import-file-info no-upload">
-                                <span>Datoteka nije prenesena</span>
+                                <span>Nije odabrana niti jedna datoteka</span>
                             </div>
                         )}
-                    </div>
+                    </ComponentBody>
 
-                    <button type="submit" className="btn btn-success" onClick={handleSubmitClick}>
-                        Uvezi
-                    </button>
+                    <ComponentFooter>
+                        <SecondaryButton onClick={handleButtonClick}>Odaberi raspored</SecondaryButton>
+                        <PrimaryButton type="submit" onClick={handleSubmitClick}>
+                            Uvezi raspored
+                        </PrimaryButton>
+                    </ComponentFooter>
                 </div>
             </div>
         </MainLayout>
