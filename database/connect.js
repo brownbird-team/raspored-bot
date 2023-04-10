15588814b4774506648e407c45fcca7124d1b7fd
rdobovic <roko@brownbird.eu>
2023-04-10 14:25:41 +0200

    Kreiram nove funkcije za upravljanje bazom - S obzirom da library koristi callback funkcije umijesto JS promise-a napravili smo   vlastite funkcije koje omogućuju korištenje library-a sa promise-ima - Stare funkcije omogućavale su korištenje samo najjednostavnijih funkcionalnosti   library-a, nove funkcije bi trebale omogućiti korištenje naprednijih funkcija

--- database/connect.js
+++ database/connect.js
@@ -6,6 +6,7 @@ const mysql = require("mysql");
 const { configCheck, getData } = require("../loadConfig.js");
 const errors = require('../errors.js');
 
+// Ispis u konzolu s prefixom [database]
 const databaseLog = async (logThis) => {
     console.log('[\u001b[35mMySQL\033[00m] ' + logThis);
 }
@@ -14,131 +15,231 @@ const databaseLog = async (logThis) => {
 // inicijalizacije
 let pool = null;
 
-// Funkcija vraća promise koji kreira connection prema mysql serveru
-const createConnection = () => {
-    return new Promise((resolve, reject) => {
-        if (!pool) {
-            reject(new errors.DatabaseError('Database needs to be initialized before it can be used, run databaseInit'));
-            return;
-        }
+// Klasa Connection služi za povezivanje s bazom podataka i izvršavanje
+// upita prema bazi
+class Connection {
 
+    // Postavi sve varijable objekta na null
+    constructor() {
+        this.fields = null;
+        this.results = null;
+        this.connection = null;
+    }
+
+    // Spoji se na bazu (kreiraj connection)
+    connect() {
+        return new Promise((resolve, reject) => {
+            // Ako pool nije aktivan javi grešku
+            if (!pool) {
+                reject(new errors.DatabaseError('Database needs to be initialized before it can be used, run databaseInit'));
+                return;
+            }
+            // Ako je ovaj connection već kreiran javi grešku
+            if (this.connection) {
+                reject(new errors.DatabaseError('This connection is already connected, release it before reconnecting it'));
+                return
+            }
+            // Inaće zatraži connection
+            pool.getConnection((err, con) => {
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to create connection on connection pool', err));
+                    return;
+                }
+                this.connection = con;
+                resolve(con);
+            });
+        });
+    }
+
+    // Izvrši neki query
+    query(...args) {
+        return new Promise((resolve, reject) => {
+            // Ako veza sa bazom nije ostvarena vrati grešku
+            if (!this.connection) {
+                reject(new errors.DatabaseError('Cannot execute query, connection is not created'));
+                return;
+            }
+            // Izvrši query
+            this.connection.query(...args, (err, results, fields) => {
+                // Ako je došlo do greške baci grešku
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to run database query', err));
+                    return;
+                }
+                // Pohrani polja i rezultate u objekt
+                this.fields = fields;
+                this.results = results;
+                // Vrati rezultate
+                resolve(results);
+            });
+        });
+    }
+
+    // Kreiraj transakciju
+    transaction() {
+        return new Promise((resolve, reject) => {
+            // Ako veza sa bazom nije ostvarena vrati grešku
+            if (!this.connection) {
+                reject(new errors.DatabaseError('Cannot create transaction, connection is not created'));
+                return;
+            }
+            // Kreiraj transakciju
+            this.connection.beginTransaction((err) => {
+                // Provjeri je li transakcija uspješno kreirana
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to create transaction', err));
+                    return
+                }
+                resolve();
+            });
+        });
+    }
+
+    // Napravi rollback (za transakcije)
+    rollback() {
+        return new Promise((resolve, reject) => {
+            // Ako veza sa bazom nije ostvarena vrati grešku
+            if (!this.connection) {
+                reject(new errors.DatabaseError('Cannot make a rollback, connection is not created'));
+                return;
+            }
+            // Napravi rollback
+            this.connection.rollback(() => {
+                resolve();
+            });
+        });
+    }
+
+    // Napravi commit (za transakcije)
+    commit() {
+        return new Promise((resolve, reject) => {
+            // Ako veza sa bazom nije ostvarena vrati grešku
+            if (!this.connection) {
+                reject(new errors.DatabaseError('Cannot commit, connection is not created'));
+                return;
+            }
+            // Napravi commit
+            this.connection.commit((err) => {
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to commit, this could be a problem', err));
+                    return;
+                }
+            });
+        });
+    }
+
+    // Otpusti ovaj connection (odspoji se)
+    release() {
+        if (this.connection) {
+            this.connection.release();
+            this.connection = null;
+        }
+    }
+
+    // Napravi ping prema serveru
+    ping() {
+        return new Promise((resolve, reject) => {
+            // Ako veza sa bazom nije ostvarena vrati grešku
+            if (!this.connection) {
+                reject(new errors.DatabaseError('Cannot ping server, connection is not created'));
+                return;
+            }
+            this.connection.ping((err) => {
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to ping the server', err));
+                    return;
+                }
+                resolve();
+            });
+        });
+    }
+
+    // Ispiši nešto u konzolu kao database
+    static log(logThis) {
+        databaseLog(logThis);
+    }
+
+    // Statična funkcija za izvršavanje upita, automatski kreira connection
+    // otpušta ga nakon izvršavanja upita
+    static query(...args) {
+        return new Promise((resolve, reject) => {
+            // Ako pool nije aktivan javi grešku
+            if (!pool) {
+                reject(new errors.DatabaseError('Database needs to be initialized before it can be used, run databaseInit'));
+                return;
+            }
+            // Zatraži connection
+            pool.getConnection((err, con) => {
+                if (err) {
+                    reject(new errors.DatabaseError('Failed to create connection on connection pool', err));
+                    return;
+                }
+                // Izvrši query
+                con.query(...args, (err, results, fields) => {
+                    // Otpusti connection
+                    con.release();
+                    // Ako je došlo do greške baci grešku
+                    if (err) {
+                        reject(new errors.DatabaseError('Failed to run database query', err));
+                        return;
+                    }
+                    // Ako je sve prošlo OK vrati rezultate
+                    resolve(results);
+                });
+            });
+        });
+    }
+}
+
+// Kreira connection pool i provjerava postojanje nužnih tablica u bazi, te
+// ih kreira po potrebi
+const databaseInit = async () => {
+    return new Promise((resolve, reject) => {
+        // Dobavi konfiguraciju za bazu
+        const database = getData().database;
+
+        // Kreiraj connection pool
+        pool = mysql.createPool({
+            connectionLimit    : database.connectionLimit,
+            host               : database.host,
+            port               : database.port,
+            user               : database.user,
+            password           : database.password,
+            database           : database.name,
+            debug              : false,
+            multipleStatements : true
+        });
+        databaseLog(`Connecting to database ${database.name} - ${database.host}:${database.port}`);
+
+        // Spoji se na bazu
         pool.getConnection((err, con) => {
             if (err) {
                 reject(new errors.DatabaseError('Failed to create connection on connection pool', err));
                 return;
             }
-            resolve(con);
-        });
-    });
-}
-exports.createConnection = createConnection;
 
-// Funkcija vraća promise koji šalje upit mysql serveru, kao argumente
-// funkciji je potrebno dati connection prema mysql serveru i string sa
-// SQL upitom
-const pureQuery = (connection, query) => {
-    return new Promise((resolve, reject) => {
-        connection.query(query, (err, res) => {
-            if (err) {
-                reject(new errors.DatabaseError('Failed to run query on given connection', err));
-                return;
-            }
-            resolve(res);
-        });
-    });
-}
-exports.pureQuery = pureQuery;
+            databaseLog("Running database initialization queries");
+            
+            // Nabavi listu file-ova sa inicijalizacijskim upitima
+            let initQuery = '';
+            const files = fs.readdirSync(__dirname + '/databaseInitQueries');
 
-// Funkcija kreira connection prema mysql serveru i izvršava callback funkciju
-// pri završetku ili grešci, sa argumentima error, redcima koji su rezultat upita
-// i poljima
-const query = (query, callback) => {
-    if (!pool)
-        return callback(new errors.DatabaseError('Database needs to be initialized before it can be used, run databaseInit'), null, null);
-
-    pool.getConnection((err, con) => {
-        if (err) {
-            return callback(new errors.DatabaseError('Failed to create connection', err), null, null);
-        } else if (con) {
-            con.query(query, (err, rows, fields) => {
-                con.release();
-                if (err) {
-                    return callback(new errors.DatabaseError('Failed to run query', err), null, null);
-                }
-                return callback(null, rows, fields);
+            // Spoji sve inicijalizacijske upite
+            files.forEach((file) => {
+                initQuery += fs.readFileSync(__dirname + '/databaseInitQueries/' + file);
             });
-        } else {
-            return callback(new errors.DatabaseError('No connection provided, i do not know what went wrong'), null, null);
-        }
-    });
-}
-exports.query = query;
 
-// Funkcija vraća promise koji kreira connection prema mysql serveru i izvršava
-// upit koji je dobila kao argument
-const promiseQuery = (query) => {
-    return new Promise((resolve, reject) => {
-        if (!pool) {
-            reject(new errors.DatabaseError('Database needs to be initialized before it can be used, run databaseInit'));
-            return;
-        }
-
-        pool.getConnection((err, con) => {
-            if (err) {
-                reject(new errors.DatabaseError('Failed to create connection for promiseQuery', err));
-                return;
-            }
-
-            con.query(query, (err, rows, fields) => {
-                con.release();
+            con.query(initQuery, (err, results, fields) => {
                 if (err) {
-                    reject(new errors.DatabaseError('Failed to run promiseQuery', err));
+                    reject(new errors.DatabaseError('Failed to run database initialization queries', err));
                     return;
                 }
-
-                resolve(rows);
+                // Sve je prošlo OK
+                resolve();
             });
         });
     });
 }
-exports.promiseQuery = promiseQuery;
 
-// Kreira connection pool i provjerava postojanje nužnih tablica u bazi, te
-// ih kreira po potrebi
-exports.databaseInit = async () => {
-    // Dobavi konfiguraciju za bazu
-    const database = getData().database;
-
-    // Kreiraj connection pool
-    pool = mysql.createPool({
-        connectionLimit    : database.connectionLimit,
-        host               : database.host,
-        port               : database.port,
-        user               : database.user,
-        password           : database.password,
-        database           : database.name,
-        debug              : false,
-        multipleStatements : true
-    });
-    databaseLog(`Connecting to database ${database.name} - ${database.host}:${database.port}`);
-
-    // Provjeri postoje li sve potrebne tablice
-    const con = await createConnection();
-    databaseLog("Running database initialization queries");
-    
-    // Nabavi listu file-ova sa inicijalizacijskim upitima
-    let initQuery = '';
-    const files = fs.readdirSync(__dirname + '/databaseInitQueries');
-
-    // Spoji sve inicijalizacijske upite
-    files.forEach((file) => {
-        initQuery += fs.readFileSync(__dirname + '/databaseInitQueries/' + file);
-    });
-
-    //let initQuery = `DELIMITER $$`;
-
-    // Pokreni inicijalizaciju (kreiraj sve što treba ako ne postoji)
-    pureQuery(con, initQuery);
-
-    return true;
-}
+// Exportaj stvari
+module.exports = { databaseInit, Connection };
