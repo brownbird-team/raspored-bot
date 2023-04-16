e8a6b414d861b88eb726db458fdbb5ea061e8ae1
rdobovic <roko@brownbird.eu>
2023-04-16 18:02:46 +0200

    Manja korekcija u frontendu

--- panel/src/data/api.js
+++ panel/src/data/api.js
@@ -1,1 +1,2 @@
-export default window.location.protocol + "//" + window.location.hostname + ":3000";
+//export default window.location.protocol + "//" + window.location.hostname + ":3000";
+export default window.location.origin
